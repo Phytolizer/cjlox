60e3091aebbdf0981b125d8fc9eae8d34d4e02e2
Kyle Coffey <mariorules24@gmail.com>
2022-01-04 19:59:25 -0600

    project structure

---
+++ modules/lox/include/lox/lox.h
@@ -0,0 +1,9 @@
+#ifndef LOX_LOX_H_
+#define LOX_LOX_H_
+
+#include <stdint.h>
+
+int32_t lox_run_file(const char *filename);
+void lox_run_prompt(void);
+
+#endif // LOX_LOX_H_
---
+++ modules/lox/source/lox.c
@@ -0,0 +1,13 @@
+#include "lox/lox.h"
+
+#include <sysexits/sysexits.h>
+
+int32_t lox_run_file(const char *filename)
+{
+	(void)filename;
+	return EX_OK;
+}
+
+void lox_run_prompt(void)
+{
+}
