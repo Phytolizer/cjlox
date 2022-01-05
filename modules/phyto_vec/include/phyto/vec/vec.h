5c97045d110a600e85154d16c0a3362431a4336b
Kyle Coffey <mariorules24@gmail.com>
2022-01-05 13:09:36 -0600

    finish veclib

--- modules/phyto_vec/include/phyto/vec/vec.h
+++ modules/phyto_vec/include/phyto/vec/vec.h
@@ -19,6 +19,7 @@ typedef struct {
     phyto_vec_shrink_mode_t shrink_mode;
     // proportion of 0x100
     uint32_t shrink_percentage;
+    int (*compare)(const void*, const void*);
 } phyto_vec_configuration_t;
 
 typedef struct {
@@ -44,14 +45,14 @@ typedef struct {
         }                                             \
     }
 
-#define PHYTO_VEC_INIT_DEFAULT(T)                               \
-    PHYTO_VEC_INIT_CONFIGURATED(                                \
-        T, ((phyto_vec_configuration_t){                        \
-               .element_size = sizeof(T),                       \
-               .minimum_size = 8,                               \
-               .shrink_mode = phyto_vec_shrink_mode_percentage, \
-               .shrink_percentage = 0x40,                       \
-           }))
+#define PHYTO_VEC_INIT_DEFAULT(T, Compare)                                              \
+    PHYTO_VEC_INIT_CONFIGURATED(T, ((phyto_vec_configuration_t){                        \
+                                       .element_size = sizeof(T),                       \
+                                       .minimum_size = 8,                               \
+                                       .shrink_mode = phyto_vec_shrink_mode_percentage, \
+                                       .shrink_percentage = 0x40,                       \
+                                       .compare = Compare,                              \
+                                   }))
 
 #define PHYTO_VEC_FREE(Vec)                                            \
     do {                                                               \
@@ -65,13 +66,55 @@ typedef struct {
         phyto_vec_push(&(Vec)->base, &val); \
     })
 
+#define PHYTO_VEC_SIZE(Vec) ((Vec)->base.size)
+
 #define PHYTO_VEC_RESIZE(Vec, NewSize) phyto_vec_resize(&(Vec)->base, NewSize)
 
 #define PHYTO_VEC_POP(Vec, OutValue) phyto_vec_pop_to(&(Vec)->base, OutValue)
+#define PHYTO_VEC_SPLICE(Vec, Index, Count) phyto_vec_splice(&(Vec)->base, Index, Count)
+#define PHYTO_VEC_SWAP_SPLICE(Vec, Index, Count) phyto_vec_swap_splice(&(Vec)->base, Index, Count)
+#define PHYTO_VEC_INSERT(Vec, Index, Value)          \
+    ({                                               \
+        typeof(Value) val = Value;                   \
+        phyto_vec_insert(&(Vec)->base, Index, &val); \
+    })
+#define PHYTO_VEC_SORT(Vec) phyto_vec_sort(&(Vec)->base)
+#define PHYTO_VEC_SWAP(Vec, IndexA, IndexB) phyto_vec_swap(&(Vec)->base, IndexA, IndexB)
+#define PHYTO_VEC_TRUNCATE(Vec, NewSize) phyto_vec_truncate(&(Vec)->base, NewSize)
+#define PHYTO_VEC_CLEAR(Vec) phyto_vec_clear(&(Vec)->base)
+#define PHYTO_VEC_RESERVE(Vec, NewCapacity) phyto_vec_reserve(&(Vec)->base, NewCapacity)
+#define PHYTO_VEC_COMPACT(Vec) phyto_vec_compact(&(Vec)->base)
+#define PHYTO_VEC_PUSH_VEC(Vec, Other) phyto_vec_push_vec(&(Vec)->base, &(Other)->base)
+#define PHYTO_VEC_PUSH_ARRAY(Vec, Array, Count) phyto_vec_push_array(&(Vec)->base, Array, Count)
+#define PHYTO_VEC_FIND(Vec, Value, OutIndex)          \
+    ({                                                \
+        typeof(Value) val = Value;                    \
+        phyto_vec_find(&(Vec)->base, &val, OutIndex); \
+    })
+#define PHYTO_VEC_REMOVE(Vec, Value)          \
+    ({                                        \
+        typeof(Value) val = Value;            \
+        phyto_vec_remove(&(Vec)->base, &val); \
+    })
+#define PHYTO_VEC_REVERSE(Vec) phyto_vec_reverse(&(Vec)->base)
 
 bool phyto_vec_resize(phyto_vec_t* vec, size_t new_size);
 bool phyto_vec_push(phyto_vec_t* vec, const void* value);
 const void* phyto_vec_pop(phyto_vec_t* vec);
 bool phyto_vec_pop_to(phyto_vec_t* vec, void* value);
+bool phyto_vec_splice(phyto_vec_t* vec, size_t index, size_t count);
+bool phyto_vec_swap_splice(phyto_vec_t* vec, size_t index, size_t count);
+bool phyto_vec_insert(phyto_vec_t* vec, size_t index, const void* value);
+bool phyto_vec_sort(phyto_vec_t* vec);
+bool phyto_vec_swap(phyto_vec_t* vec, size_t index_a, size_t index_b);
+bool phyto_vec_truncate(phyto_vec_t* vec, size_t new_size);
+void phyto_vec_clear(phyto_vec_t* vec);
+bool phyto_vec_reserve(phyto_vec_t* vec, size_t new_capacity);
+bool phyto_vec_compact(phyto_vec_t* vec);
+bool phyto_vec_push_vec(phyto_vec_t* vec, const phyto_vec_t* other);
+bool phyto_vec_push_array(phyto_vec_t* vec, const void* array, size_t count);
+bool phyto_vec_find(phyto_vec_t* vec, const void* value, size_t* out_index);
+bool phyto_vec_remove(phyto_vec_t* vec, const void* value);
+bool phyto_vec_reverse(phyto_vec_t* vec);
 
 #endif  // PHYTO_VEC_VEC_H_
--- modules/phyto_vec/source/vec.c
+++ modules/phyto_vec/source/vec.c
@@ -4,39 +4,20 @@
 #include <string.h>
 
 bool phyto_vec_resize(phyto_vec_t* vec, size_t new_size) {
-    size_t new_capacity;
-    if (new_size > vec->capacity) {
-        new_capacity = vec->capacity * 2;
-        if (new_capacity == 0) {
-            assert(vec->configuration.minimum_capacity > 0 &&
-                   "attempting to push to an uninitialized vec");
-            new_capacity = vec->configuration.minimum_capacity;
-        }
-        while (new_size > new_capacity) {
-            new_capacity *= 2;
-        }
-    } else if (new_size < vec->capacity) {
-        new_capacity = new_size;
-    } else {
-        return true;
-    }
-    uint8_t* new_data = realloc(vec->data, new_capacity);
-    if (new_data == NULL) {
+    if (!phyto_vec_reserve(vec, new_size)) {
         return false;
     }
-    vec->data = new_data;
-    vec->capacity = new_capacity;
     vec->size = new_size;
     return true;
 }
 
 bool phyto_vec_push(phyto_vec_t* vec, const void* value) {
-    if (vec->size == vec->capacity &&
-        !phyto_vec_resize(vec, vec->capacity + 1)) {
+    if (vec->size == vec->capacity && !phyto_vec_resize(vec, vec->capacity + 1)) {
         return false;
     }
 
-    memcpy(vec->data + vec->size, value, vec->configuration.element_size);
+    memcpy(vec->data + vec->size * vec->configuration.element_size, value,
+           vec->configuration.element_size);
     vec->size++;
 
     return true;
@@ -52,8 +33,7 @@ const void* phyto_vec_pop(phyto_vec_t* vec) {
         case phyto_vec_shrink_mode_never:
             break;
         case phyto_vec_shrink_mode_percentage:
-            if (vec->size * 0x100 / vec->capacity <
-                vec->configuration.shrink_percentage) {
+            if (vec->size * 0x100 / vec->capacity < vec->configuration.shrink_percentage) {
                 phyto_vec_resize(vec, vec->capacity / 2);
             }
             break;
@@ -63,7 +43,7 @@ const void* phyto_vec_pop(phyto_vec_t* vec) {
             }
             break;
     }
-    return vec->data + vec->size;
+    return vec->data + vec->size * vec->configuration.element_size;
 }
 
 bool phyto_vec_pop_to(phyto_vec_t* vec, void* value) {
@@ -72,5 +52,210 @@ bool phyto_vec_pop_to(phyto_vec_t* vec, void* value) {
         return false;
     }
     memcpy(value, popped, vec->configuration.element_size);
+    return true;
+}
+
+bool phyto_vec_splice(phyto_vec_t* vec, size_t index, size_t count) {
+    if (index >= vec->size) {
+        return false;
+    }
+    if (index + count > vec->size) {
+        count = vec->size - index;
+    }
+    if (count == 0) {
+        return true;
+    }
+    if (vec->size - count == index) {
+        vec->size -= count;
+        return true;
+    }
+    memmove(vec->data + index * vec->configuration.element_size,
+            vec->data + (index + count) * vec->configuration.element_size,
+            (vec->size - index - count) * vec->configuration.element_size);
+    vec->size -= count;
+    return true;
+}
+
+bool phyto_vec_swap_splice(phyto_vec_t* vec, size_t index, size_t count) {
+    if (index >= vec->size) {
+        return false;
+    }
+    if (index + count > vec->size) {
+        count = vec->size - index;
+    }
+    if (count == 0) {
+        return true;
+    }
+    if (vec->size - count == index) {
+        vec->size -= count;
+        return true;
+    }
+    for (size_t i = 0; i < count / 2; i++) {
+        size_t j = index + i;
+        size_t k = index + count - i - 1;
+        phyto_vec_swap(vec, j, k);
+    }
+    vec->size -= count;
+    return true;
+}
+
+bool phyto_vec_insert(phyto_vec_t* vec, size_t index, const void* value) {
+    if (index > vec->size) {
+        return false;
+    }
+    if (vec->size == vec->capacity && !phyto_vec_resize(vec, vec->size + 1)) {
+        return false;
+    }
+    memmove(vec->data + (index + 1) * vec->configuration.element_size, vec->data + index,
+            (vec->size - index) * vec->configuration.element_size);
+    memcpy(vec->data + index * vec->configuration.element_size, value,
+           vec->configuration.element_size);
+    vec->size++;
+    return true;
+}
+
+bool phyto_vec_sort(phyto_vec_t* vec) {
+    if (vec->size == 0) {
+        return true;
+    }
+    qsort(vec->data, vec->size, vec->configuration.element_size, vec->configuration.compare);
+    return true;
+}
+
+bool phyto_vec_swap(phyto_vec_t* vec, size_t index_a, size_t index_b) {
+    if (index_a >= vec->size || index_b >= vec->size) {
+        return false;
+    }
+    void* tmp = malloc(vec->configuration.element_size);
+    memcpy(tmp, vec->data + index_a * vec->configuration.element_size,
+           vec->configuration.element_size);
+    memcpy(vec->data + index_a * vec->configuration.element_size,
+           vec->data + index_b * vec->configuration.element_size, vec->configuration.element_size);
+    memcpy(vec->data + index_b * vec->configuration.element_size, tmp,
+           vec->configuration.element_size);
+    free(tmp);
+    return true;
+}
+
+bool phyto_vec_truncate(phyto_vec_t* vec, size_t new_size) {
+    if (new_size > vec->size) {
+        return false;
+    }
+    vec->size = new_size;
+    return true;
+}
+
+void phyto_vec_clear(phyto_vec_t* vec) {
+    vec->size = 0;
+}
+
+bool phyto_vec_reserve(phyto_vec_t* vec, size_t new_capacity) {
+    if (new_capacity <= vec->capacity) {
+        return true;
+    }
+    size_t actual_new_capacity;
+    if (new_capacity > vec->capacity) {
+        actual_new_capacity = vec->capacity * 2;
+        if (actual_new_capacity == 0) {
+            assert(vec->configuration.minimum_capacity > 0 &&
+                   "attempting to push to an uninitialized vec");
+            actual_new_capacity = vec->configuration.minimum_capacity;
+        }
+        while (new_capacity > actual_new_capacity) {
+            actual_new_capacity *= 2;
+        }
+    } else if (new_capacity < vec->capacity) {
+        actual_new_capacity = new_capacity;
+    } else {
+        return true;
+    }
+    uint8_t* new_data = realloc(vec->data, new_capacity * vec->configuration.element_size);
+    if (new_data == NULL) {
+        return false;
+    }
+    vec->data = new_data;
+    vec->capacity = new_capacity;
+    return true;
+}
+
+bool phyto_vec_compact(phyto_vec_t* vec) {
+    if (vec->size == vec->capacity) {
+        return true;
+    }
+    uint8_t* new_data = realloc(vec->data, vec->size * vec->configuration.element_size);
+    if (new_data == NULL) {
+        return false;
+    }
+    vec->data = new_data;
+    vec->capacity = vec->size;
+    return true;
+}
+
+bool phyto_vec_push_vec(phyto_vec_t* vec, const phyto_vec_t* other) {
+    assert(other->configuration.element_size == vec->configuration.element_size &&
+           "incompatible element sizes");
+    if (other->size == 0) {
+        return true;
+    }
+    if (vec->size + other->size > vec->capacity &&
+        !phyto_vec_reserve(vec, vec->size + other->size)) {
+        return false;
+    }
+    memcpy(vec->data + vec->size * vec->configuration.element_size, other->data,
+           other->size * vec->configuration.element_size);
+    vec->size += other->size;
+    return true;
+}
+
+bool phyto_vec_push_array(phyto_vec_t* vec, const void* array, size_t count) {
+    if (count == 0) {
+        return true;
+    }
+    if (vec->size + count > vec->capacity && !phyto_vec_reserve(vec, vec->size + count)) {
+        return false;
+    }
+    memcpy(vec->data + vec->size * vec->configuration.element_size, array,
+           count * vec->configuration.element_size);
+    vec->size += count;
+    return true;
+}
+
+bool phyto_vec_find(phyto_vec_t* vec, const void* value, size_t* out_index) {
+    if (vec->size == 0) {
+        return false;
+    }
+    size_t i = 0;
+    while (i < vec->size && vec->configuration.compare(
+                                vec->data + i * vec->configuration.element_size, value) != 0) {
+        i++;
+    }
+    if (i == vec->size) {
+        return false;
+    }
+    *out_index = i;
+    return true;
+}
+
+bool phyto_vec_remove(phyto_vec_t* vec, const void* value) {
+    size_t index;
+    if (!phyto_vec_find(vec, value, &index)) {
+        return false;
+    }
+    return phyto_vec_splice(vec, index, 1);
+}
+
+bool phyto_vec_reverse(phyto_vec_t* vec) {
+    if (vec->size == 0) {
+        return true;
+    }
+    size_t i = 0;
+    size_t j = vec->size - 1;
+    while (i < j) {
+        if (!phyto_vec_swap(vec, i, j)) {
+            return false;
+        }
+        i++;
+        j--;
+    }
     return true;
 }
